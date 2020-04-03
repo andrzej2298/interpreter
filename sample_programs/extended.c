95d3f72602e0dd8c8e99df3feee39ca45593664e
Andrzej Michnowski <am394469@students.mimuw.edu.pl>
2020-04-03 15:54:31 +0200

    add arrays to the grammar

--- sample_programs/extended.c
+++ sample_programs/extended.c
@@ -10,3 +10,14 @@ while (i < 10) {
     print(i);
     i++;
 }
+
+int[] a = {1, 2, 3, 4};
+
+int sum(int[] array) {
+    int i = 0, sum = 0;
+    while (i < len(array)) {
+        sum = sum + array[i];
+        i++;
+    }
+    return sum;
+}
